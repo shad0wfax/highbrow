ffb8370a6bd054bfe50beae1209eb4a4cf790a9c
Akshay Sharma <cto@pixelsimple.com>
2013-02-18 22:51:13 -0500

    Added more example to show modular development :)

--- build/bundle.js
+++ build/bundle.js
@@ -440,6 +440,20 @@ require.define("/app.js",function(require,module,exports,__dirname,__filename,pr
         Highbrow.mainFunc(" Hi called from app.js");
     };
 
+    Highbrow.Photo = function (options) {
+        this.options = options || {};
+        options.emailAdd =  options.emailAdd || "noadd@noadd.com";
+        options.emailLabel =  options.emailLabel || "Email adress (optional)";
+
+        console.log(options);
+        console.log("returning this - " + this);
+        return this;
+    };
+
+    Highbrow.Photo.prototype.snapPic = function() {
+        console.log(this.options);
+    }
+
 })();
 });
 
