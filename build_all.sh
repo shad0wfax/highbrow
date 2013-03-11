00163504563d7181d276ad070c7e908e2309a2a4
Akshay Sharma <cto@pixelsimple.com>
2013-03-11 10:53:36 -0400

    Renamed template prepend file and fixed build script accordingly

--- build_all.sh
+++ build_all.sh
@@ -1,33 +1,32 @@
 #!/bin/sh
 
-echo "Creating build script from the entry script using browserify"
-echo "Adding ./node_modules/.bin/ to the path, to execute commands from there."
+echo ".......................Creating build script from the entry script using browserify......................."
+echo ".......................Adding ./node_modules/.bin/ to the path, to execute commands from there........................"
 
 # PATH="./node_modules/.bin:$PATH"
 PROJECT_DIR=$(cd "$(dirname "$0")"; pwd)
 NODE_BIN=$PROJECT_DIR"/node_modules/.bin"
-echo "Nodejs binaries used from the location:  "$NODE_BIN
-
+BUILD_FILE="bundle.js"
 TEMPLATES_OUTPUT_FILE="all_templates_output.js"
-HANDLEBARS_HACK_INCLUDE_FILE="prepend_handlebars_hack"
+HANDLEBARS_HACK_INCLUDE_FILE="include_template_config.js"
 
-echo "Clean: Removing output files and will regenerate them........................"
-
+echo ".......................Nodejs binaries used from the location: $NODE_BIN......................."
+echo ".......................Clean: Removing output file. Will regenerate them........................"
 # Note - building it in both dist and public/javascripts directory:
 # dist directory is used for distribution, and public/javascripts is used for testing with brunch server
-rm ./dist/bundle.js 2>/dev/null
-rm ./public/javascripts/bundle.js 2>/dev/null
-rm ./templates/$TEMPLATES_OUTPUT_FILE
+rm ./dist/$BUILD_FILE 2>/dev/null
+rm ./public/javascripts/$BUILD_FILE 2>/dev/null
+rm ./app/templates/$TEMPLATES_OUTPUT_FILE 2>/dev/null
 
-echo "Generate templates: Running Handlebars to create a single tempalte file........................"
-
+echo ".......................Generate templates: Running Handlebars to create a single tempalte file........................"
 # Akshay hack - Added the following line to the TEMPLATES_OUTPUT_FILE in order for the variable Handlebars to be visible.
 cat ./app/templates/$HANDLEBARS_HACK_INCLUDE_FILE >  ./app/templates/$TEMPLATES_OUTPUT_FILE
 $NODE_BIN/handlebars ./app/templates/*.hbs >> ./app/templates/$TEMPLATES_OUTPUT_FILE
 
-echo "Generate build file: Running browserify to create a single dist file........................"
+echo ".......................Generate build file: Running browserify to create a single dist file........................"
+$NODE_BIN/browserify ./app/entry.js  -o ./dist/$BUILD_FILE
+$NODE_BIN/browserify ./app/entry.js  -o ./public/javascripts/$BUILD_FILE
 
-$NODE_BIN/browserify ./app/entry.js  -o ./dist/bundle.js
-$NODE_BIN/browserify ./app/entry.js  -o ./public/javascripts/bundle.js
-
-echo "build/bundle,js created. Build complete......................"
+echo ".......................Cleaning up......................."
+rm ./app/templates/$TEMPLATES_OUTPUT_FILE 2>/dev/null
+echo ".......................Build complete. The output file is at dist/$BUILD_FILE......................"
